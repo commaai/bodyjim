d6d98c20cfefb86ffd67b5f3a876a6a7e45335b0
Kacper Rączy <gfw.kra@gmail.com>
2023-11-10 19:06:34 -0800

    env changes

--- src/bodyjim/env.py
+++ src/bodyjim/env.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import gymnasium as gym
 from gymnasium import spaces
@@ -6,7 +6,8 @@ import numpy as np
 
 from bodyjim.data_stream import DataStreamSession
 
-class BaseBodyEnv(gymnasium.Env):
+
+class BaseBodyEnv(gym.Env):
   metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 20}
 
   def __init__(self, body_ip: str, cameras: List[str], services: List[str], render_mode=None):
@@ -16,7 +17,16 @@ class BaseBodyEnv(gymnasium.Env):
     self.cameras = cameras
     self.services = services
 
-    self.data_stream = None
+    self.data_stream: Optional[DataStreamSession] = None
+    self.current_state = None
+
+    self.observation_space = spaces.Dict({
+      "cameras": spaces.Dict({
+        cam: spaces.Box(low=0, high=255, shape=(480, 640, 3), dtype=np.uint8) for cam in cameras
+      }),
+      **{service: spaces.Dict() for service in services}
+    })
+    self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(2,), dtype=np.float32)
     
     assert render_mode is None or render_mode in self.metadata["render_modes"]
     self.render_mode = render_mode
@@ -24,6 +34,10 @@ class BaseBodyEnv(gymnasium.Env):
   def _initialize_data_stream(self):
     self.data_stream = DataStreamSession(cameras=self.cameras, services=self.services, webrtcd_host=self.body_ip)
     self.data_stream.start()
+
+  @property
+  def observation(self):
+
 
   def step(self, action):
     if self.data_stream is None:

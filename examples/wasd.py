1aa584e977dce7e66347127515701faa16e18359
Kacper Rączy <gfw.kra@gmail.com>
2023-12-04 21:18:27 -0800

    Add comments

--- examples/wasd.py
+++ examples/wasd.py
@@ -7,12 +7,18 @@ import pygame
 
 
 def run_wasd(body_ip, cameras):
-  env = BodyEnv(body_ip, cameras, [], render_mode="human")
+  # initialize body environment
+  # specify which cameras to stream (driver, road, wideRoad) and which services to subscribe to 
+  # supported services: https://github.com/commaai/cereal/blob/master/log.capnp
+  env = BodyEnv(body_ip, cameras, ["accelerometer", "gyroscope", "gpsLocation"], render_mode="human")
+  # reset environment to intial state
   env.reset()
 
   while True:
+    # render frame - in human rendering mode pygame window is created and updated in real time
     env.render()
 
+    # generate action from keyboard
     action = [0, 0]
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -29,7 +35,16 @@ def run_wasd(body_ip, cameras):
     if keys[pygame.K_d]:
       action[1] += -1
 
-    _, _, _, _, _ = env.step(action)
+    # observation dict contains cameras at obs["cameras"][camera_name]
+    # and other services at obs[service_name]
+    # both action and observation spaces can be randomly sampled from
+    obs, _, _, _, info = env.step(action)
+    
+    # observation dictionary structure is static, which means every field is always present, even if some messages havent been received yet.
+    # in that case they will be filled with random values (can be examined if/when received using info["timestamps"][service_name])
+    print("Acceleration:", obs["accelerometer"]["acceleration"]["v"], "updated at", info["timestamps"]["accelerometer"])
+    print("Gyroscope:", obs["gyroscope"]["gyro"]["v"], "updated at", info["timestamps"]["gyroscope"])
+    print("GPS: latitude", obs["gpsLocation"]["latitude"], "longitude", obs["gpsLocation"]["longitude"], "updated at", info["timestamps"]["gpsLocation"])
 
 
 if __name__ == "__main__":
